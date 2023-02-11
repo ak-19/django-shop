72ccf51f71be873a3833fd5cb7a2ebccf63f7e73
ante <ante.kotarac@gmail.com>
2023-02-11 21:23:20 +0100

    item details

--- shop/core/views.py
+++ shop/core/views.py
@@ -5,7 +5,6 @@ from item.models import Category, Item
 def index(request):
     items = Item.objects.filter(is_sold=False)[:6]
     categories = Category.objects.all()
-    print(items, categories)
     return render(request, 'core/index.html', {
         'categories': categories, 'items': items
     })
